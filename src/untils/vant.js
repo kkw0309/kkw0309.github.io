7f1a4a77942426c46eced154e27cba4faf8bd9a8
kkw0309 <kkw0309@163.com>
2020-07-11 15:00:39 +0800

    build

--- src/untils/vant.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue';
-import 'vant/lib/index.css';
-import { Swipe, SwipeItem , Tabbar, TabbarItem} from 'vant';
-
-Vue.use(Swipe);
-Vue.use(SwipeItem);
-Vue.use(Tabbar);
-Vue.use(TabbarItem);
